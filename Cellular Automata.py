9d8bf7e8b0475f73907dff85245228867d85cfa6
mbrowe1 <74934754+mbrowe1@users.noreply.github.com>
2022-02-27 18:34:06 -0500

    Update Cellular Automata.py
    Implemented iterations for elementary cellular automata (1-1-1) and integrated ruleset.
    
    Requires testing.

--- Cellular Automata.py
+++ Cellular Automata.py
@@ -1,23 +1,71 @@
 import numpy as np
 
-stateArray=[("***"),("** "), ("* *"), ("*  "), (" **"), (" * "),("  *"), ("   ")]
-value=[]
+#stateArray=[("***"),("** "), ("* *"), ("*  "), (" **"), (" * "),("  *"), ("   ")]
+#value=[]
 
-def arrayRules():
-    match value:
+realArray=[" "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","*"," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "] 
+
+tempArray=realArray
+
+#case = "   " 
+for len(realArray)
+    print(realArray)
+    for position in realArray
+        print(realArray)
+        
+        test = ""
+        
+        if position == 0
+           test+=(realArray[len(realArray)-1])
+           test+=(realArray[0])
+           test+=(realArray[2])
+           continue
+        elif position == (len(realArray)-1)
+           test+=(realArray[len(realArray)-2])
+           test+=(realArray[len(realArray)-1])
+           test+=(realArray[0])
+           continue
+        else
+            case+=realArray[position-1]
+            case+=realArray[position]
+            case+=realArray[postition+1]
+        
+        match value:
         case "***":
-            print(" ")
+            tempArray[position] = " "
         case "** ":
-            print(" ")
+            tempArray[position] = " "
         case "* *":
-            print(" ")
+            tempArray[position] = " "
         case "*  ":
-            print("*")
+            tempArray[position] = "*"
         case " **":
-            print("*")
+            tempArray[position] = "*"
         case " * ":
-            print("*")
+            tempArray[position] = "*"
         case "  *":
-            print("*")
+            tempArray[position] = "*"
         case "   ":
-            print(" ")
+            tempArray[position] = " "
+            
+            
+    realArray = tempArray
+
+#def arrayRules():
+#    match value:
+#        case "***":
+#            print(" ")
+#       case "** ":
+#            print(" ")
+#        case "* *":
+#            print(" ")
+#        case "*  ":
+#            print("*")
+#        case " **":
+#            print("*")
+#        case " * ":
+#            print("*")
+#        case "  *":
+#            print("*")
+#        case "   ":
+#            print(" ")
