990ff827690285be33e804825bf28e9ab0216668
mbrowe1 <74934754+mbrowe1@users.noreply.github.com>
2022-03-03 20:44:46 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string realArray[50] = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+    string tempArray[50] = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+
+    for (int i = 0; i < 50; i++) {
+        for (int x = 0; x < 50; x++)
+            cout << realArray[x];
+
+        for (int position = 0; position < 50; position++) {
+
+            string test = "";
+
+            if (position == 0) {
+                test += (realArray[0]);
+                test += (realArray[1]);
+                test += (realArray[49]);
+            }
+            else if (position == 49) {
+                test += ((realArray[position]));
+                test += (realArray[0]);
+                test += (realArray[1]);
+            }
+            else if (position == 48) {
+                test += realArray[position];
+                test += realArray[49];
+                test += realArray[0];
+            }
+            else{
+                test += realArray[position];
+                test += realArray[(position + 1)];
+                test += realArray[(position + 2)];
+            }
+
+            if (test != "   ")
+                cout << "\n test" << test;
+
+            
+            int temppos;
+            if (position == 49) {
+                temppos = position;
+                position = -1;
+            }
+                
+                
+            if (test == "***")
+                tempArray[position + 1] = " ";
+            else if (test == "** ")
+                tempArray[position + 1] = " ";
+            else if (test == "* *")
+                tempArray[position + 1] = " ";
+            else if (test == "*  ")
+                tempArray[position + 1] = "*";
+            else if (test == " **")
+                tempArray[position + 1] = "*";
+            else if (test == " * ")
+                tempArray[position + 1] = "*";
+            else if (test == "  *")
+                tempArray[position + 1] = "*";
+            else if (test == "   ")
+                tempArray[position + 1] = " ";
+            
+            if (position == -1)
+                position = temppos;
+        }
+        for (int x = 0; x < 50; x++)
+            cout << tempArray[x];
+        cout << "\n";
+    }
+}
+
