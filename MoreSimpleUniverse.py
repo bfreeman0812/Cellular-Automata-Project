a6e65e6a23f66477d6511b341454b565069655ea
mbrowe1 <74934754+mbrowe1@users.noreply.github.com>
2022-03-31 20:15:32 -0400

    Update MoreSimpleUniverse.py

--- MoreSimpleUniverse.py
+++ MoreSimpleUniverse.py
@@ -2,41 +2,36 @@ import numpy as np
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
-def generation():
-    # We are inside a function, so we must specify
-    # that we are referring to the global variable 'universe'.
-    # Otherwise python will think that we are trying to
-    # create a new variable also called 'universe'.
-    global universe
+def generation(universe):
+    
+    new_universe = np.copy(universe)
 
     # Simple loop over every possible xy coordinate.
     for i in range(universe.shape[0]):
         for j in range(universe.shape[1]):
-            survival(i, j)
+            new_universe[i,j] = survival(i, j, universe)
 
     # Set universe to be equal to new_universe.
     universe = np.copy(new_universe)
+    return universe
 
-def survival(x, y):
+def survival(x, y, universe):
     """
-    :param x: x coordinate of the cell
-    :param y: y coordinate of the cell
+    Compute one iteration of Life for one cell.
+    :param x: x coordinate of cell in the universe
+    :type x: int
+    :param y: y coordinate of cell in the universe
+    :type y: int
+    :param universe: the universe of cells
+    :type universe: np.ndarray
     """
-    # Find the number of living neighbours by taking
-    # the sum of the 8 surrounding grid squares
-    num_neighbours = np.sum(universe[x - 1:x + 2, y - 1:y + 2])
-    - universe[x, y]
-
-    # If the cell is alive
-    if universe[x, y] == 1:
-        if num_neighbours < 2 or num_neighbours > 3:
-            new_universe[x, y] = 0
-    # Otherwise, the cell is dead...
-    elif universe[x, y] == 0:
-        # ... but it will rise again
-        # IF there are three living neighbours
-        if num_neighbours == 3:
-            new_universe[x, y] = 1
+    num_neighbours = np.sum(universe[x - 1 : x + 2, y - 1 : y + 2]) - universe[x, y]
+    # The rules of Life
+    if universe[x, y] and not 2 <= num_neighbours <= 3:
+        return 0
+    elif num_neighbours == 3:
+        return 1
+    return universe[x, y]
 
 
 
@@ -50,13 +45,13 @@ beacon = [[1, 1, 0, 0],
 universe[1:5, 1:5] = beacon
 
 
-plt.imshow(universe, cmap='binary')
-plt.show()
+#plt.imshow(universe, cmap='binary')
+#plt.show()
 
 # Create an identical copy of the universe, which will be the next generation.
-new_universe = np.copy(universe)
+#new_universe = np.copy(universe)
 
-fig = plt.figure()
+fig = plt.figure(dpi=200)
 
 # Remove the axes for aesthetics
 plt.axis('off')
@@ -64,8 +59,8 @@ ims = []
 
 for i in range(30):
     # Add a snapshot of the universe, then move to the next generation
-    ims.append((plt.imshow(universe, cmap='Purples'),))
-    generation()
+    ims.append((plt.imshow(universe, cmap='binary'),))
+    universe = generation(universe)
 
 # Create the animation
 im_ani = animation.ArtistAnimation(fig, ims, interval=700,
@@ -73,3 +68,4 @@ repeat_delay=1000, blit=True)
 
 # Optional to save the animation
 im_ani.save('beacon.gif', writer="pillow")
+
